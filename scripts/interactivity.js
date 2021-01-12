69925b96ac711bedbd9ae71d245349aea244c8d8
Conjuam <Conjuam@users.noreply.github.com>
2021-01-12 18:37:50 +0000

    It's pretty much done now. Only minor tweaks from here...

--- scripts/interactivity.js
+++ scripts/interactivity.js
@@ -1,3 +1,8 @@
 function Test(){
-    alert
+    alert("Test Alert!");
+}
+
+function endOfTour()
+{
+    alert("You have now finished the whistle-stop tour of The Seven Wonders Of The Ancient World.\n\nPress <Back To Selection> to go back home.");
 }
